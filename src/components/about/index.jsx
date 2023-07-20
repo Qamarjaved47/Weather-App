d7073253cd50c63509dcd0e565c700970205972d
Qamarjaved47 <121592966+Qamarjaved47@users.noreply.github.com>
2023-07-20 14:04:30 +0500

    first commit

---
+++ src/components/about/index.jsx
@@ -0,0 +1,13 @@
+import './about.css'
+
+
+let Home = () => {
+
+    return (
+        <div  className="home">
+            This page is Notavaliable
+        </div>
+    );
+}
+
+export default Home;
---
+++ src/components/gallery/index.jsx
@@ -0,0 +1,11 @@
+import './gallery.css'
+
+let Gallery = () => {
+    return (
+        <div  className="gallery">
+            This page is Notavaliable
+        </div>
+    );
+}
+
+export default Gallery;
